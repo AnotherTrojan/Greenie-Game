04b6bc31f3a689b78999f40544a45e0cad8e013a
AnotherTrojan <AnotherTrojan@users.noreply.github.com>
2018-05-24 13:47:12 +1200

    Scene with Orthographic

---
+++ Greenie Game/Assets/Scripts/ViewSize.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSize : MonoBehaviour {
+    private Camera camera;
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+        camera.fieldOfView = 85;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
